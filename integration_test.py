42efcc25d6633c01db0578f54e07452b604275a0
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-10-06 21:05:54 +0300

    File extension and more integration tests

--- integration_test.py
+++ integration_test.py
@@ -21,7 +21,7 @@ class TestModuleIntegration(unittest.TestCase):
         self.assertFalse(data_procession.previous_state['1']['isOnline'])
 
     @patch('data_procession.requests.get')
-    def test_Should_ReturnData_When_GetDataCalledWithValidOffset(self, mock_get):
+    def test_Should_ReturnUsersOnline_When_GetDataCalledWithValidOffset(self, mock_get):
         mock_response = mock_get.return_value
         mock_response.json.return_value = {'data': [{'userId': '1', 'isOnline': True, 'lastSeenDate': '2023-10-06T08:35:30'}]}
 
@@ -32,16 +32,32 @@ class TestModuleIntegration(unittest.TestCase):
         users_online = data_procession.get_users_online(datetime.now())
         self.assertEqual(users_online, 1)
 
-        user_data = data_procession.get_user_data(datetime.now(), '1')
-        self.assertTrue(user_data['wasUserOnline'])
+    @patch('data_procession.requests.get')
+    def test_Should_ReturnCorrectPrediction_When_PredictUserCalledWithValidData(self, mock_get):
+        mock_response = mock_get.return_value
+        mock_response.json.return_value = {'data': [{'userId': '1', 'isOnline': True, 'lastSeenDate': '2023-10-06T08:35:30'}]}
 
-        prediction = data_procession.predict_users(datetime.now())
-        self.assertEqual(prediction['onlineUsers'], 1)
+        data_procession.fetch_and_update_data()
+
+        self.assertEqual(len(data_procession.previous_state), 1)
 
         user_prediction = data_procession.predict_user(datetime.now(), '1', 0.5)
         self.assertIsNotNone(user_prediction)
         self.assertIn('willBeOnline', user_prediction)
         self.assertIn('onlineChance', user_prediction)
 
-if __name__ == '__main__':
-    unittest.main()
+    @patch('data_procession.requests.get')
+    def test_Should_ReturnUsersOnlinePrediction_When_GetDataCalledWithValidOffset(self, mock_get):
+        mock_response = mock_get.return_value
+        mock_response.json.return_value = {'data': [{'userId': '1', 'isOnline': True, 'lastSeenDate': '2023-10-06T08:35:30'}]}
+
+        data_procession.fetch_and_update_data()
+
+        user_data = data_procession.get_user_data(datetime.now(), '1')
+        self.assertTrue(user_data['wasUserOnline'])
+
+        prediction = data_procession.predict_users(datetime.now())
+        self.assertEqual(prediction['onlineUsers'], 1)
+
+
+unittest.main()
